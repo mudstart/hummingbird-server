9e8df16c1d66603072f617ef2659bcef1e8fe708
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-07-06 06:15:16 -0400

    more work on amas

--- app/models/post.rb
+++ app/models/post.rb
@@ -69,6 +69,7 @@ class Post < ApplicationRecord
   validates :content, length: { maximum: 9_000 }
   validates :media, polymorphism: { type: Media }, allow_blank: true
   validates :target_user, absence: true, if: :target_group
+  validate :ama_closed
 
   def feed
     PostFeed.new(id)
@@ -118,6 +119,16 @@ class Post < ApplicationRecord
 
   def mentioned_users
     User.by_name(processed_content[:mentioned_usernames])
+  end
+
+  def ama_closed
+    return unless ama
+    return if ama.author == user
+    now_time = Time.now
+
+    unless ama.start_time <= now_time && ama.end_time >= now_time
+      errors.add(:post, 'cannot follow this ama anymore')
+    end
   end
 
   before_save do
